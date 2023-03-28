7b833a4fdde2c8925388bec82f061d962a9baf13
Al-Amin <alamincse12@gmail.com>
2023-03-28 20:25:36 +0600

    sharebus added

---
+++ testCases/test_shareBus.py
@@ -0,0 +1,23 @@
+from utilities.readProperties import ReadConfig
+from pageObjects.ShareBusPage import ShareBusPage
+import time
+
+
+class Test_ShareBus:
+
+    baseURL = ReadConfig.getApplicationURL2()
+    def test_just(self, setup):
+        self.driver = setup
+        self.driver.maximize_window()
+        self.driver.get(self.baseURL)
+        time.sleep(2)
+        self.lp = ShareBusPage(self.driver)
+        self.lp.ClickSignInButton()
+        self.lp.enterEmail("brainstation23@yopmail.com")
+        self.lp.enterPass("Pass@1234")
+        time.sleep(3)
+        self.lp.clickLoginInButton()
+        time.sleep(8)
+
+
+
